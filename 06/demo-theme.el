804ee8b622a0e5bab96adc424edd023c473a15fe
Guillaume Pasquet <dev@etenil.net>
2022-01-15 10:24:44 +0000

    Add doc for episode 06 - custom themes

---
+++ 06/demo-theme.el
@@ -0,0 +1,8 @@
+(deftheme demo "A light theme")
+
+(custom-theme-set-faces 'demo
+                        '(default ((t (:background "#FFFFFF" :foreground "#000000")))))
+
+;(add-to-list 'custom-theme-load-path "/home/etenil/Documents/emacs-livestreams/06")
+
+(provide 'demo)
